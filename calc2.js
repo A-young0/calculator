1318fc590ba7909de9a0297f64d6e512588bf932
20243119@office.deu.ac.kr <qxrt8761@gmail.com>
2026-05-27 12:02:13 +0900

    delete calc2.js file

--- calc2.js
+++
@@ -1,7 +0,0 @@
-window.onload=function(){
-    let clock = document.getElementById('clock');
-    setInterval(function() {
-        let  now = new Date();
-        clock.innerHTML=now.toString();
-    }, 1000);
-};

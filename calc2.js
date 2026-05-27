3e5185d9a0ed5d44551a9f67203eeb7b396d8dec
20243119@office.deu.ac.kr <qxrt8761@gmail.com>
2026-05-27 11:43:34 +0900

    initial commit

---
+++ calc2.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+    let clock = document.getElementById('clock');
+    setInterval(function() {
+        let  now = new Date();
+        clock.innerHTML=now.toString();
+    }, 1000);
+};
